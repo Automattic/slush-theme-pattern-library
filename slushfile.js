0fb084ccdfc63cde623d47fb799e8f945b6c6721
Daniel W. Robert <danielwrobert@gmail.com>
2015-03-17 12:21:17 -0700

    Generator: Updated slushfile to fix variable error 'path'.

--- slushfile.js
+++ slushfile.js
@@ -22,20 +22,11 @@ function format(string) {
 }
 
 var defaults = (function () {
-    var workingDirName = path.basename(process.cwd()),
-      homeDir, osUserName, configFile, user;
-
-    if (process.platform === 'win32') {
-        homeDir = process.env.USERPROFILE;
-        osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
-    }
-    else {
-        homeDir = process.env.HOME || process.env.HOMEPATH;
-        osUserName = homeDir && homeDir.split('/').pop() || 'root';
-    }
-
-    configFile = path.join(homeDir, '.gitconfig');
-    user = {};
+    var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
+        workingDirName = process.cwd().split('/').pop().split('\\').pop(),
+        osUserName = homeDir && homeDir.split('/').pop() || 'root',
+        configFile = homeDir + '/.gitconfig',
+        user = {};
 
     if (require('fs').existsSync(configFile)) {
         user = require('iniparser').parseSync(configFile).user;
