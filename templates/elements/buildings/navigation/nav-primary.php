eaba89a286938bcbf193e4a849cf53ae7adc5188
Daniel W. Robert <danielwrobert@gmail.com>
2015-03-17 22:09:11 -0700

    Includes: Restructured to mimic current Pattern Lib structure. Added conditional includes.

---
+++ templates/elements/buildings/navigation/nav-primary.php
@@ -0,0 +1,4 @@
+  <nav id="site-navigation" class="main-navigation" role="navigation">
+    <button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', '<%= appNameSlug %>' ); ?></button>
+    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+  </nav><!-- #site-navigation -->
