7f6e2ac80edf776597e9dd0f31dd9f1003b1b019
Daniel W. Robert <danielwrobert@gmail.com>
2015-06-08 17:38:18 -0400

    Moved test-files from old method into _resources dir.

--- templates/test-files/elements/buildings/navigation/nav-primary.php
+++
@@ -1,4 +0,0 @@
-  <nav id="site-navigation" class="main-navigation" role="navigation">
-    <button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', '@@appNameSlug' ); ?></button>
-    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-  </nav><!-- #site-navigation -->
