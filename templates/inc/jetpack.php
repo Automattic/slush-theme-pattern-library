d7e11c01f086cdd114216bd546f61d7c98bb72e0
sarah ✈ semark <sarah@triggersandsparks.com>
2015-10-08 17:27:31 +0100

    Fix Infinite Scroll behaviour
    Infinite Scroll was trying to use a template part that didn't exist, resulting in the posts failing to load entirely. Correcting the path here solves the problem!

--- templates/inc/jetpack.php
+++ templates/inc/jetpack.php
@@ -29,7 +29,7 @@ add_action( 'after_setup_theme', '<%= appNameVar %>_jetpack_setup' );
 function <%= appNameVar %>_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
-		get_template_part( 'template-parts/content', get_post_format() );
+		get_template_part( 'components/content', get_post_format() );
 	}
 } // end function <%= appNameVar %>_infinite_scroll_render
 
